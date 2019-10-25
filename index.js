ad14ab1e8092aa35cceeba23652f5fb421a0f113
Gamuro <kapa6@tut.by>
2019-10-25 21:03:48 +0300

    add mongodb

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const WebSocket = require('ws');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const bodyParser = require('body-parser');
+
 
 const port = process.env.PORT || 3000;
-
+var db;
 const app = express();
-const server = app.listen(port, () => {
+const server = app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended: true}))
+    .listen(port, () => {
     console.log('Server started on port', port);
 });
+
+
+
+MongoClient.connect('mongodb+srv://chat:43214321@chatdb-am2y3.gcp.mongodb.net/test?retryWrites=true&w=majority', (err, database)=>{
+    if(err){
+        return console.log(err);
+    }
+    db = database.db('angrychatdb');
+   
+})
+
 
 const wss = new WebSocket.Server({ server });
 
@@ -18,6 +36,31 @@ wss.on('connection', (ws) => {
             }
         });
     });
-  
+
 });
+
+
+
+
+
+// --------------------------------API -----------------------------
+
+app.get('/', (_, res)=>{
+    res.send('Welcome to chat api');
+});
+
+app.post('/users', (req, res)=>{
+    const user = req.body;
+    db.collection('users').findOne({ name: user.name }, (err, result)=>{
+        if(err){
+            return res.sendStatus(500);
+        }
+        if(!result){
+            db.collection('users').insertOne(user);
+            res.sendStatus(200);
+        }else{
+            res.sendStatus(501);
+        }
+    })
+})
 
