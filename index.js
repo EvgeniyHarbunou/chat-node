9a2f93ae48186275bf867d0c9e1d5e551fb88dfc
Gamuro <kapa6@tut.by>
2019-10-25 19:22:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const WebSocket = require('ws');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+const server = app.listen(port, () => {
+    console.log('Server started on port', port);
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+  
+});
+
